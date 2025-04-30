45b3b3d5d93670ba70c58d00948e9a95d89d1d36
Fizzrss <fizars123@gmail.com>
2025-04-30 14:36:23 +0700

    percobaan2

--- stackTugasMahasiswa.java
+++ stackTugasMahasiswa.java
@@ -69,4 +69,17 @@ public class stackTugasMahasiswa {
         }
         return x;
     }
+    public String konversiDesimalKeBiner(int nilai){
+        stackKonversi09 stack = new stackKonversi09();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai/2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
 }
