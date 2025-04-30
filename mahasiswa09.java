1988a4d0c6648bfe1bc830e021ba10c339a75875
Fizzrss <fizars123@gmail.com>
2025-04-30 12:38:28 +0700

    jobsheet9

---
+++ mahasiswa09.java
@@ -0,0 +1,21 @@
+public class mahasiswa09 {
+
+    String nim;
+    String nama;
+    String kelas;
+    int nilai;
+
+    mahasiswa09(){
+
+    }
+
+    mahasiswa09(String nama, String nim, String kelas){
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+    }
+
+    void tugasDinilai(int nilai){
+        this.nilai = nilai;
+    }
+}
---
+++ stackTugasMahasiswa.java
@@ -0,0 +1,57 @@
+public class stackTugasMahasiswa {
+    mahasiswa09[] stack;
+    int top;
+    int size;
+
+    public stackTugasMahasiswa(int size){
+        this.size = size;
+        stack = new mahasiswa09[size];
+        top = -1;
+    }
+    public boolean isFull(){
+        if (top == size) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean isEmpty(){
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void push(mahasiswa09 mhs){
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("stack penuh! Tidak bisa menambahkan tugas lagi");
+        }
+    }
+    public mahasiswa09 pop(){
+        if (!isEmpty()) {
+            mahasiswa09 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+            return null;
+        }
+    }
+    public mahasiswa09 peek(){
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan.");
+            return null;
+        }
+    }
+    public void print(){
+        for (int i = 0; i < top; i++) {
+            System.out.println(stack[i].nama + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+}
