7967cf7a8ddfd2d9fa79f2ae54f18007aaa63d99
Fizzrss <fizars123@gmail.com>
2025-06-11 16:47:04 +0700

    Sorting

--- praktikum14/demoArrayLIst.java
+++ praktikum14/demoArrayLIst.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class demoArrayLIst {
     public static void main(String[] args) {
@@ -30,6 +31,18 @@ public class demoArrayLIst {
             System.out.println(cust.toString());
         }
 
+        System.out.println(customers);
+
+        System.out.println("\n--- Praktikum Sorting ---");
+        ArrayList<String> daftarSiswa = new ArrayList<>();
+        daftarSiswa.add("Zainab");
+        daftarSiswa.add("Andi");
+        daftarSiswa.add("Rara");
+        Collections.sort(daftarSiswa);
+        System.out.println(daftarSiswa);
+
+        System.out.println("--- collection of objects ---");
+        customers.sort((c1,c2)->c1.name.compareTo(c2.name));
         System.out.println(customers);
     }  
 }
